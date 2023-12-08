54bb82a00f400eccfddf983c5f9b4cb2a3afc2ee
40741122 <z3848095@gmail.com>
2023-12-08 15:51:19 +0800

    coupon-api-pdo done

---
+++ coupon-pdo-version/css.php
@@ -0,0 +1,12 @@
+<!-- Bootstrap CSS v5.2.1 -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+
+
+<link rel="stylesheet" href="../css/style.css">
